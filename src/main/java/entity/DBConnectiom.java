40af5d623a5a5db4ae1b25a6d6a2d7df464e7707
d19920527 <d362514@gmail.com>
2025-11-12 22:29:26 +0800

    DB更改Drivrt

--- src/main/java/entity/DBConnectiom.java
+++ src/main/java/entity/DBConnectiom.java
@@ -8,7 +8,7 @@ public class DBConnectiom {
 	public static Connection getConnection() throws Exception {
 		
 		Connection conn= null;
-		
+		//MySQL Drivate
 		String Drivate = "com.mysql.cj.jdbc.Driver";
 		String url = "jdbc:mysql://localhost:3306/spring_client?useSSL=false&serverTimezone=Asia/Taipei";
 				// sql8.0.13版本後除了資料庫位址外，接著輸入一個問號並且要加上資料庫所在的時區
