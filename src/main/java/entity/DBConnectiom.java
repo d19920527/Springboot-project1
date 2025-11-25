d950d0b8a7f6dddcdd1b4288707b1c8ac7d66a56
d19920527 <d362514@gmail.com>
2025-11-25 23:23:11 +0800

    2025-11-25 桌機遠端連線筆電DB 架構完成

--- src/main/java/entity/DBConnectiom.java
+++ src/main/java/entity/DBConnectiom.java
@@ -10,10 +10,10 @@ public class DBConnectiom {
 		Connection conn= null;
 		//MySQL Drivate
 		String Drivate = "com.mysql.cj.jdbc.Driver";
-		String url = "jdbc:mysql://localhost:3306/spring_client?useSSL=false&serverTimezone=Asia/Taipei";
+		String url = "jdbc:mysql://192.168.100.130:3306/spring_client?useSSL=false&serverTimezone=Asia/Taipei";
 				// sql8.0.13版本後除了資料庫位址外，接著輸入一個問號並且要加上資料庫所在的時區
 				String user = "root";
-				String password = "1234567890";
+				String password = "123456";
 				Class.forName(Drivate);
 				conn=  DriverManager.getConnection(url, user, password);
 				return conn;
